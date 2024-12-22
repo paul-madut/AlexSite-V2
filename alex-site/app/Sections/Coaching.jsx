be4f77b3989f13aaf7102eac49e0df5c551c78b4
Paul Madut <paul.madut@mths.ca>
2024-12-21 20:37:37 -0500

    contact text update

--- alex-site/app/Sections/Coaching.jsx
+++ alex-site/app/Sections/Coaching.jsx
@@ -103,14 +103,14 @@ function Coaching() {
                   className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
                 />
                 <Image
-                  src="https://assets.aceternity.com/features-section.png"
+                  src="https://files.autoblogging.ai/images/personal-trainers-toolkit-enhancing-client-engagement-with-sms-marketing(w3ql).jpg"
                   alt="feature template"
                   width={500}
                   height={500}
                   className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
                 />
                 <Image
-                  src="https://assets.aceternity.com/pro/bento-grids.png"
+                  src="https://photos.peopleimages.com/picture/202301/2588153-fitness-man-and-phone-call-with-water-bottle-on-bench-with-smile-for-conversation-after-workout-at-gym.-happy-sporty-male-on-smartphone-in-communication-while-taking-a-break-from-intense-training-zoom_90.jpg"
                   alt="bento template"
                   width={500}
                   height={500}
--- alex-site/app/Sections/Contact.jsx
+++ alex-site/app/Sections/Contact.jsx
@@ -11,11 +11,7 @@ function Contact() {
       </h1>
       <p></p>
       <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
-        Welcome to MailJet, the best transactional email service on the web.
-        We provide reliable, scalable, and customizable email solutions for
-        your business. Whether you&apos;re sending order confirmations,
-        password reset emails, or promotional campaigns, MailJet has got you
-        covered.
+        Join Now!
       </p>
       <input
         type="text"
